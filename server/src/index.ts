2035f3510c7073740a016c4a5b5eecd326b1caf1
m4gie <steeven.regnault@epitech.eu>
2020-04-12 22:08:42 +0200

    add(server): typescript, socketio, nodemon

---
+++ server/src/index.ts
@@ -0,0 +1,7 @@
+import io from "socket.io";
+const server = io.listen(3000);
+
+server.on("connection", function (socket) {
+  console.log("user connected");
+  socket.emit("welcome", "welcome man");
+});
