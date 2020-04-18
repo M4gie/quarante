edc846811615491b7196866df914777ec1953b9a
m4gie <steeven.regnault@epitech.eu>
2020-04-18 16:01:05 +0200

    add(server): console log connect/disconnect

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,17 @@
 import io from 'socket.io';
+
 const server = io.listen(3000);
 
 server.on('connection', function (socket) {
-  console.log('user connected');
-  socket.emit('welcome', 'welcome man');
+  console.log('New connection !');
+  server.emit('this', { will: 'Is connected' });
+
+  socket.on('private message', function (from, msg) {
+    console.log('I received a private message by ', from, ' saying ', msg);
+  });
+
+  socket.on('disconnect', function () {
+    console.log('User disconnected !');
+    server.emit('user disconnected');
+  });
 });
