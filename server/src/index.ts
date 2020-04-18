940ecc5e092342e6471cd0daf347fe5452bbbf24
m4gie <steeven.regnault@epitech.eu>
2020-04-18 16:56:00 +0200

    add(server): send number of connected clients

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,17 @@ import io from 'socket.io';
 
 const server = io.listen(3000);
 
-server.on('connection', function (socket) {
-  console.log('New connection !');
-  server.emit('this', { will: 'Is connected' });
+let clients = 0;
 
-  socket.on('private message', function (from, msg) {
-    console.log('I received a private message by ', from, ' saying ', msg);
-  });
+server.on('connection', function (socket) {
+  /* Easy way to count client, .clients doesn't send the "real" value */
+  clients++;
+  console.log('User connected !');
+  server.emit('connected', clients);
 
   socket.on('disconnect', function () {
+    clients--;
     console.log('User disconnected !');
-    server.emit('user disconnected');
+    server.emit('connected', clients);
   });
 });
