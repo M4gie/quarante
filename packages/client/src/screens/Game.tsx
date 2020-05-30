fa3a10c2f5d2f43d795302e52656381f5c9c3b25
m4gie <steeven.regnault@epitech.eu>
2020-05-30 18:21:18 +0200

    add(web): game screen

---
+++ packages/client/src/screens/Game.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text } from 'react-native-paper';
+import { useRecoilState, useRecoilValue } from 'recoil';
+
+import CenterContainer from '../components/CenterContainer';
+import GameInput from '../components/GameInput';
+import answerState from '../global/answer';
+import questionState from '../global/question';
+
+export default function Game() {
+  const [isQuestionTime] = useState(false);
+  const answer = useRecoilValue(answerState);
+  const question = useRecoilState(questionState);
+
+  return (
+    <CenterContainer>
+      <View style={styles.info}>
+        <Text style={{ fontSize: 20 }}>
+          {!isQuestionTime && answer !== '' && `La réponse était: ${answer}`}
+        </Text>
+      </View>
+      <View style={styles.info}>
+        <Text style={{ fontSize: 30 }}>{question}</Text>
+      </View>
+      <GameInput />
+    </CenterContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  info: {
+    marginBottom: 'auto',
+    marginTop: 10,
+  },
+});
