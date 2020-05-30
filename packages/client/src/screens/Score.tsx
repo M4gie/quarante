1f5bfc5d66135a224c4373b9f725c47c2682616c
m4gie <steeven.regnault@epitech.eu>
2020-05-30 14:28:07 +0200

    add(web): score screen

---
+++ packages/client/src/screens/Score.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Button from '../components/Button';
+import CenterContainer from '../components/CenterContainer';
+
+export default function Score() {
+  return (
+    <CenterContainer>
+      <Button>Score</Button>
+    </CenterContainer>
+  );
+}
