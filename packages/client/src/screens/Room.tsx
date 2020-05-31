ec43fffa018f1fbfac5c3064502eadb1592d2a29
m4gie <steeven.regnault@epitech.eu>
2020-05-31 19:51:09 +0200

    add(web): gameTab component

--- packages/client/src/screens/Room.tsx
+++ packages/client/src/screens/Room.tsx
@@ -1,33 +1,19 @@
 import { useFocusEffect } from '@react-navigation/native';
 import React from 'react';
-import { Dimensions } from 'react-native';
-import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import { useRecoilState } from 'recoil';
 import io from 'socket.io-client';
 
+import GameTab from '../components/GameTab';
 import getEnv from '../constant/index';
 import socketState from '../global/socket';
 import { HomeNavigatorProps } from '../typings/navigation';
 import { useScreenWidth } from '../utils/hooks/screenWidth';
 import Game from './Game';
-import Score from './Score';
-
-const initialLayout = { width: Dimensions.get('window').width };
 
 export default function Room({ route, navigation }: HomeNavigatorProps<'Room'>) {
-  const isLargeScreen = useScreenWidth();
   navigation.setOptions({ headerTitle: route.params.title });
+  const isLargeScreen = useScreenWidth();
   const [socket, setSocket] = useRecoilState(socketState);
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: 'first', title: 'Jeu' },
-    { key: 'second', title: 'Score' },
-  ]);
-
-  const renderScene = SceneMap({
-    first: Game,
-    second: Score,
-  });
 
   useFocusEffect(
     React.useCallback(() => {
@@ -41,23 +27,5 @@ export default function Room({ route, navigation }: HomeNavigatorProps<'Room'>) 
     return <Game />;
   }
 
-  return (
-    <TabView
-      renderTabBar={RenderTabBar}
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
-      onIndexChange={setIndex}
-      initialLayout={initialLayout}
-    />
-  );
+  return <GameTab />;
 }
-
-const RenderTabBar = (props: any) => {
-  return (
-    <TabBar
-      {...props}
-      indicatorStyle={{ backgroundColor: 'white' }}
-      style={{ backgroundColor: '#272745' }}
-    />
-  );
-};
