edb0e482cee951736e32057703c91326ce951543
m4gie <steeven.regnault@epitech.eu>
2020-07-11 10:08:53 +0200

    fix(client): scoreboard is not longer place with absolute
    #7

--- packages/client/src/screens/Game.tsx
+++ packages/client/src/screens/Game.tsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { useTheme } from 'react-native-paper';
 
-import CenterContainer from '../components/CenterContainer';
 import GameAnswer from '../components/GameAnswer';
 import GameInput from '../components/GameInput';
 import GameQuestion from '../components/GameQuestion';
 import { LargeScoreBoard } from '../components/ScoreBoard';
 
 export default function Game() {
+  const { colors } = useTheme();
   return (
-    <CenterContainer>
+    <View style={[styles.container, { backgroundColor: colors.primary }]}>
       <LargeScoreBoard />
-      <View style={styles.info}>
-        <GameAnswer />
+      <View style={styles.gameContainer}>
+        <View style={styles.info}>
+          <GameAnswer />
+        </View>
+        <View style={styles.info}>
+          <GameQuestion />
+        </View>
+        <GameInput />
       </View>
-      <View style={styles.info}>
-        <GameQuestion />
-      </View>
-      <GameInput />
-    </CenterContainer>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  gameContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   info: {
     marginBottom: 'auto',
     marginTop: 10,
