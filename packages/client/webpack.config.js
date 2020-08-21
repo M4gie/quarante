e7efe0c9dfd886e574a4009819473934e3dc25f4
m4gie <steeven.regnault@epitech.eu>
2020-08-21 16:56:33 +0200

    feat: 0.4.0

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -2,7 +2,7 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = async (env, argv) => {
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync({ ...env, offline: false }, argv);
 
   // Optionally you can enable the bundle size report.
   // It's best to do this only with production builds because it will add noticeably more time to your builds and reloads.
