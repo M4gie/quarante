842aa288f8ad91bc9fbbcfad774e247dfcff5a8d
m4gie <steeven.regnault@epitech.eu>
2020-09-22 12:13:45 +0200

    feat(client): add a footer to join our discord and twitter
    #60

--- packages/client/src/components/CenterContainer.tsx
+++ packages/client/src/components/CenterContainer.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import { StyleSheet, StyleProp, ViewStyle, View } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
+import Footer from './Footer';
+
 type CenterContainerProps = {
   children: React.ReactNode;
+  footerEnable?: boolean;
   style?: StyleProp<ViewStyle>;
 };
 
-export default function CenterContainer({ style, ...props }: CenterContainerProps) {
+export default function CenterContainer({
+  style,
+  children,
+  footerEnable,
+  ...props
+}: CenterContainerProps) {
   const { colors } = useTheme();
-  return <View style={[styles.container, { backgroundColor: colors.primary }, style]} {...props} />;
+
+  return (
+    <View style={{ flex: 1, backgroundColor: colors.primary }} {...props}>
+      <View style={[styles.container, style]}>{children}</View>
+      <Footer enable={footerEnable === undefined ? false : footerEnable} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
